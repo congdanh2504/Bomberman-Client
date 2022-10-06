9a8b81be93522a24f1ee4b35aae1b491b917f486
congdanh2504 <danhuchiha2002@gmail.com>
2022-10-06 22:50:28 +0700

    init

---
+++ scripts/Menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var input_username = $VBoxContainer/Username
+onready var input_ip = $VBoxContainer/IP
+
+func validate():
+	Global.set_serve_ip(input_ip.text)
+	Global.set_username(input_username.text)
+	return get_tree().change_scene("res://scenes/Rooms.tscn")
+
+func _on_Button_pressed():
+	validate()
